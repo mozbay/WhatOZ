72b2f293a917ff056983cd11e3e20e5a7d28bbb5
sencergultuna <sencergultuna@gmail.com>
2018-04-15 23:46:45 +0300

    -- action added

--- app/src/main/java/com/whatoz/application/aphorism/MainActivity.java
+++ app/src/main/java/com/whatoz/application/aphorism/MainActivity.java
@@ -3,6 +3,12 @@ package com.whatoz.application.aphorism;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by muratozbay on 13.04.2018.
@@ -10,9 +16,63 @@ import android.support.annotation.Nullable;
 
 public class MainActivity extends Activity {
 
+    EditText password;
+    EditText userName;
+    CheckBox rememberMe;
+    TextView forgetPassword;
+    Button login;
+    TextView newUser;
+
+    public static final String TAG = "MainActivity";
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+
+        password = (EditText) findViewById(R.id.password);
+        userName = (EditText) findViewById(R.id.user_name);
+        rememberMe = (CheckBox) findViewById(R.id.checkBox);
+        forgetPassword = (TextView) findViewById(R.id.forget_password);
+        login = (Button) findViewById(R.id.login_button);
+        newUser = (TextView) findViewById(R.id.new_user);
+
+        newUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: yeni kayıt sayfasına yönlendir
+                Log.e(TAG,"yeni kullanıcı ekleye tıklandı");
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: kullanıcıyı kontrol edip aforizmanın olacağı sayfaya yönlendir
+                Log.e(TAG,"logine tıklandı");
+            }
+        });
+
+        rememberMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(rememberMe.isChecked()) {
+                    // TODO: direk aforizma sayfasına yönlendir
+                    Log.e(TAG,"checkbox check edildi");
+                }
+            }
+        });
+
+
+
+        forgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: mail isteyip yeni şifreyi mail atma sayfasına yönlendir
+                Log.e(TAG,"şifremi unuttuma tıklandı");
+            }
+        });
     }
 }
