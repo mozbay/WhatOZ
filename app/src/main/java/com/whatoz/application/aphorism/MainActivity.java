8a727575c3b4670e4a9e5686c0ac3d44220e6179
muratozbay <muratozbay89@gmail.com>
2018-04-15 23:18:28 +0300

    - bugfixed

--- app/src/main/java/com/whatoz/application/aphorism/MainActivity.java
+++ app/src/main/java/com/whatoz/application/aphorism/MainActivity.java
@@ -1,15 +1,14 @@
 package com.whatoz.application.aphorism;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
 /**
  * Created by muratozbay on 13.04.2018.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
