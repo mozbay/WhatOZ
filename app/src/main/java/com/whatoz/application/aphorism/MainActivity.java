df64c20d0b8e40c4e6b6f7a4bd72322dfcc1058f
muratozbay <muratozbay89@gmail.com>
2018-04-15 21:56:12 +0300

    - Main Activity added

---
+++ app/src/main/java/com/whatoz/application/aphorism/MainActivity.java
@@ -0,0 +1,12 @@
+package com.whatoz.application.aphorism;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by muratozbay on 13.04.2018.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+
+}
